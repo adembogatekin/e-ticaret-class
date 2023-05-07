4f01af6f8975a88df083f26185c4e65aa7cd99fc
adembogatekin <pektekin99@gmail.com>
2023-05-07 09:31:35 +0300

    07.05.2023

--- src/components/chart/Chart.js
+++ src/components/chart/Chart.js
@@ -1,10 +1,48 @@
 //// admin panelindeki home componentinin içindeki order status chart kısmı
-import React from 'react'
+import React from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+import Card from '../card/Card';
+import styles from "./Chart.module.scss"
+import { useSelector } from 'react-redux';
+import { selectOrderHistory } from '../../redux/slice/orderSlice';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
+    },
+    title: {
+      display: false,
+      text: 'Chart.js Bar Chart',
+    },
+  },
+};
 
 const Chart = () => {
-  return (
-    <div>Chart</div>
-  )
-}
 
-export default Chart
+  const orders = useSelector(selectOrderHistory)
+
+  const array = []
+  orders.map((item) => {
+    const {orderStatus} = item
+    return array.push(orderStatus)
+  })
