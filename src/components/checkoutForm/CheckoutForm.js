7bded9bb3789176ec63cfc1c17c59c5de2f0b3a2
adembogatekin <pektekin99@gmail.com>
2023-05-10 21:33:33 +0300

    10.05.2023

--- src/components/checkoutForm/CheckoutForm.js
+++ src/components/checkoutForm/CheckoutForm.js
@@ -1,10 +1,24 @@
 //// cart sayfasından checkout sayfasına ulaşıp, checkout detaylarını girip, proceed to checkout a bastığımızda karşımıza çıkan iki card a sahip check out componenti. checkoutsummary componenti de ayrı bir dosyada oluşturulmuştur.
-import React from 'react'
-
-const CheckoutForm = () => {
-  return (
-    <div>CheckoutForm</div>
-  )
-}
-
-export default CheckoutForm
+import React, { useEffect, useState } from "react";
+import {
+  PaymentElement,
+  LinkAuthenticationElement,
+  useStripe,
+  useElements,
+} from "@stripe/react-stripe-js";
+import styles from "./CheckoutForm.module.scss";
+import Card from "../card/Card";
+import CheckoutSummary from "../checkoutSummary/CheckoutSummary";
+import spinnerImg from "../../assets/spinner.gif";
+import { toast } from "react-toastify";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { selectEmail, selectUserID } from "../../redux/slice/authSlice";
+import {
+    CLEAR_CART,
+  selectCartItems,
+  selectCartTotalAmount,
+} from "../../redux/slice/cartSlice";
+import { selectShippingAddress } from "../../redux/slice/checkoutSlice";
+import { addDoc, collection, Timestamp } from "firebase/firestore";
+import { db } from "../../firebase/config";
