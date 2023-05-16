7eb3370708d32ca73dd410d3566dad24b68e0d04
adembogatekin <pektekin99@gmail.com>
2023-05-16 17:34:32 +0300

    16.05.2023

--- src/components/slider/Slider.js
+++ src/components/slider/Slider.js
@@ -1,9 +1,14 @@
 //// home sayfasındaki slider i yapacağımız yer
 import React from 'react'
+import "./Slider.scss"
+import {AiOutlineArrowLeft, AiOutlineArrowRight} from "react-icons/ai"
 
 const Slider = () => {
   return (
-    <div>Slider</div>
+    <div classNa me="slider">
+      <AiOutlineArrowLeft className="arrow prev"/>
+      <AiOutlineArrowRight className="arrow right"/>
+    </div>
   )
 }
 
