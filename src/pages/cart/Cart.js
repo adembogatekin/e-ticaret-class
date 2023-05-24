61c60495a43d29bda3304993c249125e2a9816ed
adembogatekin <pektekin99@gmail.com>
2023-05-24 21:43:20 +0300

    24.05.2023

--- src/pages/cart/Cart.js
+++ src/pages/cart/Cart.js
@@ -1,5 +1,13 @@
 //// alışveriş sepeti (cart) sayfası
-import React from 'react'
+//// alışveriş sepeti (cart) sayfası
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Link, useNavigate } from 'react-router-dom'
+import { ADD_TO_CART, CALCULATE_SUBTOTAL, CALCULATE_TOTAL_QUANTITY, CLEAR_CART, DECREASE_CART, REMOVE_FROM_CART, SAVE_URL, selectCartItems, selectCartTotalAmount, selectCartTotalQuantity } from '../../redux/slice/cartSlice'
+import styles from "./Cart.module.scss"
+import {FaTrashAlt} from "react-icons/fa"
+import Card from '../../components/card/Card'
+import { selectIsLoggedIn } from '../../redux/slice/authSlice'
 
 const Cart = () => {
   return (
