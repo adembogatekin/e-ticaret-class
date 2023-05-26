50bfdb0de34e03153f85324cd6b6c3fd001974c8
adembogatekin <pektekin99@gmail.com>
2023-05-26 23:32:00 +0300

    26.05.2023

--- src/components/admin/home/Home.js
+++ src/components/admin/home/Home.js
@@ -1,5 +1,16 @@
 ////admin panelindeki home componenti
-import React from 'react'
+////admin panelindeki home componenti
+import React, { useEffect } from 'react'
+import InfoBox from '../../infoBox/InfoBox'
+import styles from "./Home.module.scss"
+import {AiFillDollarCircle} from "react-icons/ai"
+import { FaCartArrowDown } from 'react-icons/fa'
+import {BsCart4} from "react-icons/bs"
+import { useDispatch, useSelector } from 'react-redux'
+import { selectProducts, STORE_PRODUCTS } from '../../../redux/slice/productSlice'
+import { CALC_TOTAL_ORDER_AMOUNT, selectOrderHistory, selectTotalOrderAmount, STORE_ORDERS } from '../../../redux/slice/orderSlice'
+import useFetchCollection from '../../../customHooks/useFetchCollection'
+import Chart from '../../chart/Chart'
 
 const Home = () => {
   return (
