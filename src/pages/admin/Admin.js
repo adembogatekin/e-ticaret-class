342541d5d117c0cd3df356ff7d38da0c47a591d7
adembogatekin <pektekin99@gmail.com>
2023-06-01 23:04:12 +0300

    01.06.2023

--- src/pages/admin/Admin.js
+++ src/pages/admin/Admin.js
@@ -1,32 +1,41 @@
 //// admin sayfası
 import React from 'react'
-import {Route, Routes} from "react-router-dom"
-import styles from "./Admin.module.scss"
+import { Route, Routes } from 'react-router-dom'
 import Navbar from '../../components/admin/navbar/Navbar'
 import Home from '../../components/admin/home/Home'
+import styles from "./Admin.module.scss"
 import ViewProducts from '../../components/admin/viewProducts/ViewProducts'
 import AddProduct from '../../components/admin/addProduct/AddProduct'
 import Orders from '../../components/admin/orders/Orders'
 import OrderDetails from '../../components/admin/orderDetails/OrderDetails'
 
-
-const Admin = () => { 
+const Admin = () => {
   return (
     <div className={styles.admin}>
       <div className={styles.navbar}>
+        {/* admin panelindeki sol navbar */}
         <Navbar/>
       </div>
       <div className={styles.content}>
         <Routes>
-          <Route path="Home" element={<Home/>}/>
+
+          {/* admin panelindeki home componenti */}
+          <Route path="home" element={<Home/>}/>
+
+          {/* admin panelindeki allproducts componenti */}
           <Route path="all-products" element={<ViewProducts/>}/>
-          <Route path="add-product" element={<AddProduct/>}/>
-          <Route path="order" element={<Orders/>}/>
-          <Route path="order-details" element={<OrderDetails/>}/>
+
+          {/* admin panelindeki add product componenti */}
+          <Route path="add-product/:id" element={<AddProduct/>}/>
+
+          {/* admin panelindeki orders componenti */}
+          <Route path="orders" element={<Orders/>}/>
+
+          {/* admin panelindeki orders componentinin içindeki order statusa tıklayınce çıkam order details componenti */}
+          <Route path="order-details/:id" element={<OrderDetails/>}/>
+
         </Routes>
-
       </div>
-
     </div>
   )
 }
